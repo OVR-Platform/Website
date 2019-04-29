9302e6007ec11942a701718f405eb3d1b26869c7
Antonio Dal Cin <antoniodalcin94@gmail.com>
2019-04-29 16:15:21 +0200

    ntc

--- src/js/build/main.js
+++ src/js/build/main.js
@@ -11,9 +11,62 @@ import mapboxgl from 'mapbox-gl'
 import geojson2h3 from 'geojson2h3'
 const h3 = require("h3-js");
 import inView from 'in-view'
+import PinchZoom from 'pinch-zoom-js'
+
+
 const body = $1("body")
 var is_safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent) && screen.width > 1000;
 
+const fetchSVG = async function (url, el) {
+  // Dog bless fetch() and await, though be advised you'll need
+  // to transpile this down to ES5 for older browsers.
+  let response = await fetch(url);
+  let data = await response.text();
+
+  // This response should be an XML document we can parse.
+  const parser = new DOMParser();
+  const parsed = parser.parseFromString(data, 'image/svg+xml');
+
+  // The file might not actually begin with "<svg>", and
+  // for that matter there could be none, or many.
+  let svg = parsed.getElementsByTagName('svg');
+  if (svg.length) {
+    // But we only want the first.
+    svg = svg[0];
+
+    // Copy over the attributes first.
+    const attr = svg.attributes;
+    const attrLen = attr.length;
+    for (let i = 0; i < attrLen; ++i) {
+      if (attr[i].specified) {
+        // Merge classes.
+        if ('class' === attr[i].name) {
+          const classes = attr[i].value.replace(/\s+/g, ' ').trim().split(' ');
+          const classesLen = classes.length;
+          for (let j = 0; j < classesLen; ++j) {
+            el.classList.add(classes[j]);
+          }
+        }
+        // Add/replace anything else.
+        else {
+          el.setAttribute(attr[i].name, attr[i].value);
+        }
+      }
+    }
+
+    // Now transfer over the children. Note: IE does not
+    // assign an innerHTML property to SVGs, so we need to
+    // go node by node.
+    while (svg.childNodes.length) {
+      el.appendChild(svg.childNodes[0]);
+    }
+    // console.log('fetch done');
+    setTimeout(() => {
+      // console.log('ehe')
+      Application.svgInit();
+    }, 2000)
+  }
+};
 
 let final_list = [];
 
@@ -281,6 +334,20 @@ Application.mapInit();
 }, 250))
 
 window.addEventListener('load', () => {
+  const svgs = document.querySelectorAll('svg[data-url]');
+  const svgsLen = svgs.length;
+  // Loop and process.
+  for (let i = 0; i < svgsLen; ++i) {
+   
+    // Grab the URL and delete the attribute; we no longer
+    // need it.
+    let url = svgs[i].getAttribute('data-url');
+    svgs[i].removeAttribute('data-url');
+
+    // We'll let another function handle the actual fetching
+    // so we can use the async modifier.
+    fetchSVG(url, svgs[i]);
+  }
 })
 
 
@@ -310,7 +377,8 @@ const Application = (() => {
       imagesLoaded: true,
       adaptiveHeight: true,
       hash: true,
-      pauseAutoPlayOnHover: false
+      pauseAutoPlayOnHover: false,
+      draggable: false
     })
 
     const featuresButton = $('.js-feature-tab-button')
@@ -347,7 +415,7 @@ const Application = (() => {
   }
 
   function mailchimpCallback (data) {
-    console.log(data)
+    // console.log(data)
   }
 
   const menuManager = () => {
@@ -380,7 +448,7 @@ const Application = (() => {
     var timer = Countdown.timer(end, function(timeLeft) {
       document.getElementById("c-countdown_container").innerHTML = "Presale will start in "+timeLeft.days + " days " + timeLeft.hours + " hours " + timeLeft.minutes + " min " + timeLeft.seconds + " sec ";
     }, function() {
-      console.log("Countdown Finished!")
+      // console.log("Countdown Finished!")
     })
   }
 
@@ -672,6 +740,55 @@ const Application = (() => {
   
   }
 
+  const fadeInTimelineS = () => {
+    TweenMax.set(['#LINEA_1 path', 
+    '#LINEA_2 path', 
+    '#LINEA_3 path',
+    '#LINEA_4 path',
+    '#LINEA_5 path',
+    '#LINEA_6 path',
+    '#LINEA_7 path',
+    '#LINEA_8',
+    '#LINEA_9 path',
+    '#LINEA_10 path',
+    '#LINEA_11 path',
+    ], {opacity: 0}); // hex mondo
+
+    var timelineSCGTL = new TimelineMax({repeat:-1});
+    timelineSCGTL.timeScale(9);
+    timelineSCGTL.addPause(400).play();
+
+    inView('#timeline-svg').once('enter', function(){
+      var tl = new TimelineMax({delay: 1, repeat:-1});
+      tl.staggerTo([
+        '#LINEA_1 path', 
+        '#LINEA_2 path', 
+        '#LINEA_3 path',
+        '#LINEA_4 path',
+        '#LINEA_5 path',
+        '#LINEA_6 path',
+        '#LINEA_7 path',
+        '#LINEA_8',
+        '#LINEA_9 path',
+        '#LINEA_10 path',
+        '#LINEA_11 path',
+      ], 0.4, {opacity:1}, 0.05)
+      .add(timelineSCGTL, 0)
+    });
+  }
+
+  const pinchZoom = () => {
+    if (screen.width < 769 ){
+      let options = {
+        tapZoomFactor: 2,
+        draggableUnzoomed: false
+
+      }
+      let el = document.querySelector('#ecosystem');
+      let pz = new PinchZoom(el, options);
+    }
+  }
+
   const init = () => {
     scrollToLink();
     startParallax();
@@ -683,12 +800,18 @@ const Application = (() => {
     preloaderFadeOut();
     mapInit();
     switchFooterColor();
+    pinchZoom();
+  }
+  
+  const svgInit = () => {
     fadeInScndS();
+    fadeInTimelineS();
   }
 
   return {
     init,
     mapInit: mapInit,
+    svgInit,
   }
 })()
 
